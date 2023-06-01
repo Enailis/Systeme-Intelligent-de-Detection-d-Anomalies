5dcb5f024ee03ad5c7c2c892b9d76098a2c3a8ab
Gildas <gildas@ecobosto.fr>
2023-06-01 11:01:54 +0200

    Import

---
+++ test.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import os
+
+ot_odr_filename = os.path.join(".", "data", "OT_ODR.csv.bz2")
+ot_odr_df = pd.read_csv(ot_odr_filename,
+                        compression="bz2",
+                        sep=";")
+
+equipements_filename = os.path.join(".", "data", 'EQUIPEMENTS.csv')
+equipements_df = pd.read_csv(equipements_filename,
+                             sep=";")
